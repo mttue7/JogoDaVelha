354fb37170e66401610f6fb21261169238c9a7c4
Matheus Anitelli <mattanitelli4@gmail.com>
2023-11-22 22:09:32 -0300

    O começo

---
+++ JogoVelha.java
@@ -0,0 +1,11 @@
+public class JogoVelha{
+
+    public static void main(String[] args){
+
+        char[][] board = {{' ', ' ', ' '}, 
+                          {' ', ' ', ' '} , 
+                          {' ', ' ', ' '}};
+        
+    }
+
+}
